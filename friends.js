cabcdd5e568015ca3cb500737475f61ada686a1e
Adriane Sutherland <asutherland91@gmail.com>
2022-12-07 17:38:00 -0800

    Add a function

--- friends.js
+++ friends.js
@@ -2,3 +2,7 @@ var arrays =  ["Adriane", "Stacy", "Jacob", "Peter", "Hanna"]
 console.log(arrays[2])
 console.log(arrays[4])
 console.log(arrays[0])
+
+var places = ["Israel", "Greece", "Ghana", "Italy", "Japan"]
+places.pop();
+console.log(places)
